4d9c8fe8f57ed445da726a2461ed65694e9ea682
Sebastien Binet <binet@cern.ch>
2012-10-31 16:52:36 +0100

    added ATLAS-oriented instructions

---
+++ README.md
@@ -0,0 +1,16 @@
+gaudi-hive
+==========
+
+A testbed for a multicore-enabled gaudi-based framework.
+
+Installation
+------------
+
+```sh
+$ . ./atlas-setup.sh
+$ ./configure
+$ cd build-dir
+$ make -j 8
+$ make install
+```
+
---
+++ atlas-setup.sh
@@ -0,0 +1,4 @@
+export CMTCONFIG=${CMTCONFIG:-x86_64-slc5-gcc46-opt}
+export CMTPROJECTPATH=${CMTPROJECTPATH:-/afs/cern.ch/sw/lcg/app/releases}
+export PATH=/afs/cern.ch/sw/lcg/external/CMake/2.8.6/$CMTCONFIG/bin:$PATH
+. /afs/cern.ch/sw/lcg/external/gcc/4.6.2/${CMTCONFIG}/setup.sh
