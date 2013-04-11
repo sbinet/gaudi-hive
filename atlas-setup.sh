4b44282fa45755361d039f423f19ea3b2a1381b2
Sebastien Binet <binet@cern.ch>
2013-04-11 10:01:37 +0200

    atlas-setup: use x86_64-slc6-gcc46-opt as default

--- atlas-setup.sh
+++ atlas-setup.sh
@@ -1,4 +1,4 @@
-export CMTCONFIG=${CMTCONFIG:-x86_64-slc5-gcc46-opt}
+export CMTCONFIG=${CMTCONFIG:-x86_64-slc6-gcc46-opt}
 export CMTPROJECTPATH=${CMTPROJECTPATH:-/afs/cern.ch/sw/lcg/app/releases}
 export PATH=/afs/cern.ch/sw/lcg/external/CMake/2.8.6/$CMTCONFIG/bin:$PATH
 
